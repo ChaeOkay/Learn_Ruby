08c9418cafd3f7ca67086c672acd5869eaaeabc1
cokeefe <chaemobile@gmail.com>
2013-06-22 17:36:14 -0400

    rspec caps

--- 04_pig_latin.rb
+++ 04_pig_latin.rb
@@ -1,8 +1,22 @@
 def translate(words_to_pig)
-  pig_words = words_to_pig.split(" ")
+
+  if words_to_pig != words_to_pig.downcase
+    words_to_pig.downcase
+    words_array = pig_latinize(words_to_pig).map do |word|
+      word.capitalize!
+    end
+  else
+    words_array = pig_latinize(words_to_pig)
+  end
+  words_array.join(" ")
+end
+
+
+def pig_latinize(words)
+  pig_words = words.split(" ")
 
   pig_words.map! do |word|
-	  if word.start_with?('a', 'e', 'i', 'o')
+    if word.start_with?('a', 'e', 'i', 'o')
       word = word + "ay"
     else
       word_start = word[/^[^aeio]+/]
@@ -10,5 +24,5 @@ def translate(words_to_pig)
       word = word.insert(-1, word_start+"ay")
     end
   end
-  pig_words.join(" ")
+  pig_words
 end
