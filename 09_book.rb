984de9455733b695d43ae82e32670a5adef9550a
cokeefe <chaemobile@gmail.com>
2013-07-02 14:31:12 -0400

    finish 09

--- 09_book.rb
+++ 09_book.rb
@@ -1,8 +1,20 @@
 class Book
-attr_accessor :title
+
+  attr_accessor :title
+
+  def initialize
+    @lowercase_words = ['the', 'a', 'an', 'and', 'in', 'of'] 
+    @title_new = ''
+  end
 
   def title
-    @title.capitalize
+
+    @title.split(' ').map do |word| 
+      @title_new += @lowercase_words.include?(word) ? "#{word}\s" : "#{word.capitalize}\s"
+    end
+
+    @title_new[0] = @title_new.slice(0, 1).capitalize
+    @title = @title_new[0...-1]
   end
 
 end
